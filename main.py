fa4c16d95ac6eee7810cd3038602164f4cb7a661
Mohamed Abdelrazek <45569039+mohamedabdelrazek9@users.noreply.github.com>
2022-08-18 00:30:05 +0200

    [RHGN]: add test

--- main.py
+++ main.py
@@ -71,6 +71,7 @@ args = parser.parse_known_args()[0]
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
 networkx_format_list = ['.graphml', '.gexf', '.gml', '.leda', '.net']
+print(args.dataset_path)
 data_extension = os.path.splitext(args.dataset_path)[1]
 
 def FairGNN_pre_processing(data_extension):
