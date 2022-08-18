89787f02b68f1a8811e8bb6b0b374e8a26415acd
Mohamed Abdelrazek <45569039+mohamedabdelrazek9@users.noreply.github.com>
2022-08-18 20:15:48 +0200

    add missing column name in df_click

--- alibaba_processing/ali_RHGN_pre_processing.py
+++ alibaba_processing/ali_RHGN_pre_processing.py
@@ -18,7 +18,7 @@ def ali_RHGN_pre_process(df):
     df_item.dropna(axis=0, subset=['cate_id', 'campaign_id', 'brand'], inplace=True)
     df_item.rename(columns={'adgroup_id':'pid', 'cate_id':'cid'}, inplace=True)
 
-    df_click.rename(columns={'user':'uid', 'adgroup_id':'pid'}, inplace=True)
+    df_click.rename(columns={'userid':'uid', 'adgroup_id':'pid'}, inplace=True)
     df_click = df_click[df_click['clk']>0]
 
     df_click.drop('clk', axis=1, inplace=True)
