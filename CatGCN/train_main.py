52e6863dfe6045e3de6bcb7b246974dda1a8d74f
Mohamed Abdelrazek <45569039+mohamedabdelrazek9@users.noreply.github.com>
2022-08-25 22:12:00 +0200

    [CatGCN]: comment neptune for now

--- CatGCN/train_main.py
+++ CatGCN/train_main.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-import neptune.new as neptune
+#import neptune.new as neptune
 
 #from parser import parameter_parser
 from CatGCN.clustering import ClusteringMachine
