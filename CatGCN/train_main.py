a7eeed089041960c8999483e2ece8130b5bd845c
Mohamed Abdelrazek <45569039+mohamedabdelrazek9@users.noreply.github.com>
2022-08-30 21:13:44 +0200

    adjust sensitive attribute calculation

--- CatGCN/train_main.py
+++ CatGCN/train_main.py
@@ -55,7 +55,7 @@ def train_CatGCN(user_edge, user_field, user_gender, user_labels, seed, label, a
     gnn_trainer.train_val_test()
 
     ## Compute accuracy per sensitive attribute group
-    if args.dataset_name == 'nba' or args.dataset_name == 'pokec':
+    if args.dataset_name == 'nba' or args.dataset_name == 'pokec_z' or args.dataset_name == 'pokec_n':
         pos_preds_distr = pos_preds_attr_distr(user_labels, gnn_trainer.targets, gnn_trainer.predictions, clustering_machine.sg_test_nodes[0], label, args.sens_attr)
     else:
         pos_preds_distr = pos_preds_attr_distr(user_labels, gnn_trainer.targets, gnn_trainer.predictions, clustering_machine.sg_test_nodes[0], label, "age")
