2f3516d636c7c71f6f3cfcfdfc68184d7301ce14
Mohamed Abdelrazek <45569039+mohamedabdelrazek9@users.noreply.github.com>
2022-09-03 12:59:59 +0200

    change cuda name

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ def FairGNN_pre_processing(data_extension):
     
     G = dgl.DGLGraph()
     #G.from_scipy_sparse_matrix(adj)
-    G = dgl.from_scipy(adj, device='cuda:0')
+    G = dgl.from_scipy(adj, device='cuda')
     
     if args.dataset_name == 'nba':
         features = feature_norm(features)
