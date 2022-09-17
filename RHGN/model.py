d4d7320e4d778acbc7695eb54de2f245c478960a
Mohamed Abdelrazek <45569039+mohamedabdelrazek9@users.noreply.github.com>
2022-09-17 19:50:49 +0200

    add test

--- RHGN/model.py
+++ RHGN/model.py
@@ -26,7 +26,7 @@ class RHGN_adv(nn.Module):
         self.cid3_feature.weight.requires_grad = False
 
         self.adv_model = nn.Linear(n_hid, n_out)
-        #self.sens_model = GCN() #nfeat, hidden_units, 1, dropout
+        #self.sens_model = GCN(,n_hidd, 1, 0.5) #nfeat, hidden_units, 1, dropout
         #self.optimizer_A = torch.optim.Adam(self.adv_model.parameters(), lr=0.1, weight_decay=1e-5)
         #self.A_loss = 0
 
@@ -76,6 +76,7 @@ class ali_RHGN(nn.Module):
         self.key = nn.Linear(200, n_inp)
         self.value = nn.Linear(200, n_inp)
         self.skip = nn.Parameter(torch.ones(1))
+        print('G:', G.shape)
 
         #self.query_sens = nn.Linear(200, n_inp)
         #self.key_sens = nn.Linear(200, n_inp)
@@ -120,7 +121,7 @@ class ali_RHGN(nn.Module):
         # brand_feature = blocks[0].srcnodes['brand'].data['inp']
 
         inputs=torch.cat((cid1_feature,cid2_feature,cid3_feature),1)        #(N,4,200)
-        print(inputs.shape)
+        print('inputs:', inputs.shape) # (455, 3, 200)
         k = self.key(inputs) #(N,4,n_inp)
         v = self.value(inputs) #(N,4,n_inp)
         q = self.query(item_feature.unsqueeze(-2)) #(N,1,n_inp)
