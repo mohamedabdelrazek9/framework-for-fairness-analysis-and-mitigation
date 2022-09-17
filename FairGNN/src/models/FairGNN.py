dfb9c27c7e38a42abffcb5563aba5b7b97ff0b93
Mohamed Abdelrazek <45569039+mohamedabdelrazek9@users.noreply.github.com>
2022-09-17 12:15:17 +0200

    add simple test #2

--- FairGNN/src/models/FairGNN.py
+++ FairGNN/src/models/FairGNN.py
@@ -61,6 +61,9 @@ class FairGNN(nn.Module):
 
         s_score = torch.sigmoid(s.detach())
         # s_score = (s_score > 0.5).float()
+        print('s_score:', s_score.shape)
+        print('idx_sens_tran:', idx_sens_train.shape)
+        print('sens:', sens.shape)
         s_score[idx_sens_train]=sens[idx_sens_train].unsqueeze(1).float()
         y_score = torch.sigmoid(y)
         # Lr
