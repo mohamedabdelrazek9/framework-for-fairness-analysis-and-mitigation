7e627e88a46fbdaa3242ee08e990e2ca070eae7b
Mohamed Abdelrazek <45569039+mohamedabdelrazek9@users.noreply.github.com>
2022-10-08 14:09:18 +0200

    change parameter

--- CatGCN/train_main.py
+++ CatGCN/train_main.py
@@ -58,7 +58,7 @@ def train_CatGCN(user_edge, user_field, user_gender, user_labels, seed, label, a
     if args.dataset_name == 'nba' or args.dataset_name == 'pokec_z' or args.dataset_name == 'pokec_n':
         pos_preds_distr = pos_preds_attr_distr(user_labels, gnn_trainer.targets, gnn_trainer.predictions, clustering_machine.sg_test_nodes[0], label, args.sens_attr)
     else:
-        pos_preds_distr = pos_preds_attr_distr(user_labels, gnn_trainer.targets, gnn_trainer.predictions, clustering_machine.sg_test_nodes[0], label, "age")
+        pos_preds_distr = pos_preds_attr_distr(user_labels, gnn_trainer.targets, gnn_trainer.predictions, clustering_machine.sg_test_nodes[0], label, args.sens_attr)
     print(pos_preds_distr)
     #neptune_run["pos_preds_distr"] = pos_preds_distr
 
