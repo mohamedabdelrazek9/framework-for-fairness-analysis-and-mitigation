366ed0d37a3d13035cb4e3c9f26549142a03875d
Mohamed Abdelrazek <45569039+mohamedabdelrazek9@users.noreply.github.com>
2022-10-25 21:33:44 +0200

    add print

--- RHGN/jd_main.py
+++ RHGN/jd_main.py
@@ -233,6 +233,7 @@ def tecent_training_main(G, cid1_feature, cid2_feature, cid3_feature, cid4_featu
     #G=torch.load('{}/{}.pkl'.format(args.data_dir,args.graph))
     print(G)
     labels=G.nodes['user'].data[label]
+    print('labels:', labels)
 
 
     # generate train/val/test split
