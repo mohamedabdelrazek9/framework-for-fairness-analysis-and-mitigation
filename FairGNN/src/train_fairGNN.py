bbc9cfbdf2d66f579df4b20f7f12c5d8361a3be1
Mohamed Abdelrazek <45569039+mohamedabdelrazek9@users.noreply.github.com>
2022-10-30 00:27:37 +0200

    clean up

--- FairGNN/src/train_fairGNN.py
+++ FairGNN/src/train_fairGNN.py
@@ -49,11 +49,11 @@ def train_FairGNN(G, features, labels, idx_train, idx_val, idx_test, sens, idx_s
         acc_val = accuracy(output[idx_val], labels[idx_val])
         roc_val = roc_auc_score(labels[idx_val].cpu().numpy(),output[idx_val].detach().cpu().numpy())
         f1_val = f1_score(labels[idx_val].cpu().numpy(), (output[idx_val].squeeze()>0).type_as(labels).cpu().numpy())
-        print('F1:', f1_val)
-        print('output:', output[idx_val])
-        print('labels:', labels[idx_val])
-        print('output numpy:', output[idx_val].detach().cpu().numpy())
-        print('labels numpy:', labels[idx_val].cpu().numpy())
+        #print('F1:', f1_val)
+        #print('output:', output[idx_val])
+        #print('labels:', labels[idx_val])
+        #print('output numpy:', output[idx_val].detach().cpu().numpy())
+        #print('labels numpy:', labels[idx_val].cpu().numpy())
         #f1_val = f1_score(labels[idx_val].cpu().numpy(), output[idx_val].detach().cpu().numpy(), average='binary')
 
 
@@ -63,7 +63,7 @@ def train_FairGNN(G, features, labels, idx_train, idx_val, idx_test, sens, idx_s
 
         acc_test = accuracy(output[idx_test], labels[idx_test])
         roc_test = roc_auc_score(labels[idx_test].cpu().numpy(),output[idx_test].detach().cpu().numpy())
-        #f1_test = f1_score(labels[idx_val].cpu().numpy(), output[idx_val].detach().cpu().numpy(), average='binary')
+        f1_test = f1_score(labels[idx_test].cpu().numpy(), (output[idx_test].squeeze()>0).type_as(labels).cpu().numpy())
         parity,equality, oae_diff, te_diff = fair_metric(output,idx_test, labels, sens)
         if acc_val > args.acc and roc_val > args.roc:
         
@@ -72,7 +72,7 @@ def train_FairGNN(G, features, labels, idx_train, idx_val, idx_test, sens, idx_s
 
                 best_result['acc'] = acc_test.item()
                 best_result['roc'] = roc_test
-                #best_result['F1'] = f1_test
+                best_result['F1'] = f1_test
                 best_result['parity'] = parity
                 best_result['equality'] = equality
                 best_result['accuracy equality'] = oae_diff
@@ -86,7 +86,7 @@ def train_FairGNN(G, features, labels, idx_train, idx_val, idx_test, sens, idx_s
                 'adv: {:.4f}'.format(adv_loss.item()),
                 'acc_val: {:.4f}'.format(acc_val.item()),
                 "roc_val: {:.4f}".format(roc_val),
-                #"F1_val: {:.4f}".format(f1_val),
+                "F1_val: {:.4f}".format(f1_val),
                 "parity_val: {:.4f}".format(parity_val),
                 "equality: {:.4f}".format(equality_val),
                 "accuracy equality: {:.4f}".format(oae_diff_val),
@@ -94,7 +94,7 @@ def train_FairGNN(G, features, labels, idx_train, idx_val, idx_test, sens, idx_s
             print("Test:",
                     "accuracy: {:.4f}".format(acc_test.item()),
                     "roc: {:.4f}".format(roc_test),
-                    #"F1: {:.4f}".format(f1_test),
+                    "F1: {:.4f}".format(f1_test),
                     "acc_sens: {:.4f}".format(acc_sens),
                     "parity: {:.4f}".format(parity),
                     "equality: {:.4f}".format(equality),
