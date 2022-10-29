638a8eb8de6b930b88e9197fff690768ab21907e
Mohamed Abdelrazek <45569039+mohamedabdelrazek9@users.noreply.github.com>
2022-10-30 00:15:00 +0200

    add test f1

--- FairGNN/src/train_fairGNN.py
+++ FairGNN/src/train_fairGNN.py
@@ -48,6 +48,8 @@ def train_FairGNN(G, features, labels, idx_train, idx_val, idx_test, sens, idx_s
         output,s = model(G, features)
         acc_val = accuracy(output[idx_val], labels[idx_val])
         roc_val = roc_auc_score(labels[idx_val].cpu().numpy(),output[idx_val].detach().cpu().numpy())
+        f1_val = f1_score(labels[idx_val].cpu().numpy(), (output[idx_val].squeeze()>0).type_as(labels).cpu().numpy())
+        print('F1:', f1_val)
         print('output:', output[idx_val])
         print('labels:', labels[idx_val])
         print('output numpy:', output[idx_val].detach().cpu().numpy())
