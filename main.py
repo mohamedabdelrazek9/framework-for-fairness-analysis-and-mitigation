56650df9e16b98305caec523fdf1515b18888114
Mohamed Abdelrazek <45569039+mohamedabdelrazek9@users.noreply.github.com>
2022-11-07 19:05:11 +0100

    adjust test

--- main.py
+++ main.py
@@ -280,7 +280,7 @@ def CatGCN_pre_processing(data_extension):
             df = pd.read_csv(args.dataset_path)
             #df_edge_list = pd.read_csv('./region_job_relationship.txt', sep=" ", header=None)
             #df_edge_list = pd.read_csv('./region_job_relationship.txt', delimiter= "\t", header=None)
-            df_edge_list = pd.read_csv('./region_job_relationship.txt', delimiter= "\t", header=None)
+            df_edge_list = pd.read_csv('../region_job_relationship.txt', delimiter= "\t", header=None)
             df_edge_list.rename(columns={0: "source", 1: "target"}, inplace=True)
             #df_edge_list = pd.read_csv('../user_edge.csv')
 
