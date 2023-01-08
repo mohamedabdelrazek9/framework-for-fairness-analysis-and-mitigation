0412ca27f21e22c83dd3884c07674a541c67cd55
Mohamed Abdelrazek <45569039+mohamedabdelrazek9@users.noreply.github.com>
2023-01-08 16:51:17 +0100

    fix bug

--- main.py
+++ main.py
@@ -199,10 +199,11 @@ def FairGNN_pre_processing(data_extension):
             elif args.dataset_name == 'pokec_z':
                 df_nodes = pd.read_csv(args.dataset_path)
                 edges_path = '../region_job_relationship'
+                df_edge_list = edges_path
                 df_edge_list.to_csv(r'{}.txt'.format(edges_path), header=None, index=None, sep=' ', mode='a')
             #save the edges as .txt file
             #edges_path = './FairGNN_data_relationship'
-            #df_edge_list.to_csv(r'{}.txt'.format(edges_path), header=None, index=None, sep=' ', mode='a')
+            # df_edge_list.to_csv(r'{}.txt'.format(edges_path), header=None, index=None, sep=' ', mode='a')
         else:
             # simple test for pokec/tecent
             df_nodes = pd.read_csv(args.dataset_path)    
