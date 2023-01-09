7501de16f02b6e06cc4ac3bf03c5a2134cc2fedd
Mohamed Abdelrazek <45569039+mohamedabdelrazek9@users.noreply.github.com>
2023-01-09 16:27:59 +0100

    fix bug #3

--- main.py
+++ main.py
@@ -198,9 +198,10 @@ def FairGNN_pre_processing(data_extension):
                 df_edge_list = edges_path
             elif args.dataset_name == 'pokec_z':
                 df_nodes = pd.read_csv(args.dataset_path)
-                edges_path = '../MasterThesis-dev/region_job_relationship.txt'
-                df_edge_list = edges_path
-                df_edge_list.to_csv(r'{}.txt'.format(edges_path), header=None, index=None, sep=' ', mode='a')
+                edges_path = '../Master-Thesis-dev/region_job_relationship.txt'
+                #df_edge_list = edges_path
+                #df_edge_list = pd.read_csv('../Master-Thesis-dev/region_job_relationship.txt')
+                #df_edge_list.to_csv(r'{}.txt'.format(edges_path), header=None, index=None, sep=' ', mode='a')
                 #df = pd.read_csv('../Master-Thesis-dev/region_job.csv')
                 #df_edge_list = pd.read_csv('../region_job_relationship.txt',  sep="\t", header=None)
                 #edges_path = ''
