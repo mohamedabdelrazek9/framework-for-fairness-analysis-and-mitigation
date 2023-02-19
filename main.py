cfbcaefe32acd71d6c32f2bb6ff59065327a7fd6
Mohamed Abdelrazek <45569039+mohamedabdelrazek9@users.noreply.github.com>
2023-02-19 11:52:59 +0100

    add condition for 2 models

--- main.py
+++ main.py
@@ -494,6 +494,9 @@ if args.type == 1:
         cat_pre_processing = CatGCN_pre_processing(data_extension)
     if 'RHGN' in args.model_type:
         rhgn_pre_processing = RHGN_pre_processing(data_extension)
+    if 'FairGNN' in args.model_type and 'RHGN' in args.model_type:
+        fair_pre_processing = FairGNN_pre_processing(data_extension)
+        rhgn_pre_processing = RHGN_pre_processing(data_extension)
 
 #if 'FairGNN' in args.model_type:
 #    fair_pre_processing = FairGNN_pre_processing(data_extension)
